5f72f9c332b3b15ccd8d37d0a5d103d7a8efd915
vijaybdr25 <168228278+vijaybdr25@users.noreply.github.com>
2024-08-22 16:30:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CollegeList from './components/CollegeList';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sportsdunia</h1>
+      <CollegeList />
+    </div>
+  );
+}
+
+export default App;
